f76453124b1ef27bbb05383a690de3578c33a62f
niharchaudhri <37373980+niharchaudhri@users.noreply.github.com>
2018-03-18 19:07:33 +0530

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+ $db_name = "employee101";
+ $mysql_username = "root";
+ $mysql_password = "";
+ $server_name = "www.github.com";
+ $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
+?>
